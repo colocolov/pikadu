6abd0db6c3e0ef2696b82597d9d9922320956457
colocolov <46902522+colocolov@users.noreply.github.com>
2020-10-23 13:42:53 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let menuToggle = document.querySelector('#menu-toggle');
+let menu = document.querySelector('.sidebar');
+
+menuToggle.addEventListener('click',  function(event){
+  console.log('Клик по кнопке');
+  event.preventDefault();
+  menu.classList.toggle('visible');
+
+})
